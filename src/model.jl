b607e47850dd946c8c13a720e431285183576b44
pat-alt <altmeyerpat@gmail.com>
2023-02-14 08:17:19 +0100

    uh

--- src/model.jl
+++ src/model.jl
@@ -6,14 +6,14 @@ using SliceMap
 using Statistics
 
 """
-    Models.ConformalModel <: AbstractDifferentiableJuliaModel
+    ConformalModel <: Models.AbstractDifferentiableJuliaModel
 
 Constructor for models trained in `Flux.jl`. 
 """
-struct Models.ConformalModel <: AbstractDifferentiableJuliaModel
+struct ConformalModel <: Models.AbstractDifferentiableJuliaModel
     model::ConformalPrediction.ConformalProbabilisticSet
     likelihood::Symbol
-    function FluxModel(model, likelihood)
+    function ConformalModel(model, likelihood)
         if likelihood ∈ [:classification_binary, :classification_multi]
             new(model, likelihood)
         else
@@ -27,16 +27,16 @@ struct Models.ConformalModel <: AbstractDifferentiableJuliaModel
 end
 
 # Outer constructor method:
-function Models.ConformalModel(model; likelihood::Symbol=:classification_binary)
-    Models.ConformalModel(model, likelihood)
+function ConformalModel(model; likelihood::Symbol=:classification_binary)
+    ConformalModel(model, likelihood)
 end
 
 # Methods
-function logits(M::Models.ConformalModel, X::AbstractArray)
+function logits(M::ConformalModel, X::AbstractArray)
     return SliceMap.slicemap(x -> M.model(x), X, dims=(1, 2))
 end
 
-function probs(M::Models.ConformalModel, X::AbstractArray)
+function probs(M::ConformalModel, X::AbstractArray)
     if M.likelihood == :classification_binary
         output = σ.(logits(M, X))
     elseif M.likelihood == :classification_multi
