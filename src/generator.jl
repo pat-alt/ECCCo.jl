7b672ec1233779e3ddb23ba6b1a7bd4bd0a4f0d3
Pat Alt <55311242+pat-alt@users.noreply.github.com>
2023-04-04 14:27:02 +0200

    progres on granular examination

--- src/generator.jl
+++ src/generator.jl
@@ -1,7 +1,7 @@
 using CounterfactualExplanations.Objectives
 
 "Constructor for `CCEGenerator`."
-function CCEGenerator(; λ::Union{AbstractFloat,Vector{<:AbstractFloat}}=[0.1, 1.0], κ::Real=0.0, temp::Real=0.05, kwargs...)
+function CCEGenerator(; λ::Union{AbstractFloat,Vector{<:AbstractFloat}}=[0.1, 1.0], κ::Real=1.0, temp::Real=0.05, kwargs...)
     function _set_size_penalty(ce::AbstractCounterfactualExplanation)
         return CCE.set_size_penalty(ce; κ=κ, temp=temp)
     end
