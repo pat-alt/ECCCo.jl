80ec8764b4cf296fd32e37cd61d08bdd180fbcf9
pat-alt <altmeyerpat@gmail.com>
2023-04-18 15:40:00 +0200

    almost there

--- src/CCE.jl
+++ src/CCE.jl
@@ -9,9 +9,6 @@ include("losses.jl")
 include("generator.jl")
 include("sampling.jl")
 
-using MLJFlux
-MLJFlux.reformat(X, ::Type{<:AbstractMatrix}) = X'
-
 export CCEGenerator, EnergySampler, set_size_penalty, distance_from_energy
 
 end
