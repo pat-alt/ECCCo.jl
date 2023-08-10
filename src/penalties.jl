335f7133f0f0515686120db93f0eb3bcba1c39f9
Pat Alt <55311242+pat-alt@users.noreply.github.com>
2023-08-10 11:52:24 +0200

    :fire:

--- src/penalties.jl
+++ src/penalties.jl
@@ -38,6 +38,53 @@ function set_size_penalty(
 
 end
 
+function energy_delta(
+    ce::AbstractCounterfactualExplanation;
+    n::Int=50, niter=500, from_buffer=true, agg=mean,
+    choose_lowest_energy=true,
+    choose_random=false,
+    nmin::Int=25,
+    return_conditionals=false,
+    kwargs...
+)
+
+    _loss = 0.0
+    nmin = minimum([nmin, n])
+
+    @assert choose_lowest_energy ⊻ choose_random || !choose_lowest_energy && !choose_random "Must choose either lowest energy or random samples or neither."
+
+    conditional_samples = []
+    ignore_derivatives() do
+        _dict = ce.params
+        if !(:energy_sampler ∈ collect(keys(_dict)))
+            _dict[:energy_sampler] = ECCCo.EnergySampler(ce; niter=niter, nsamples=n, kwargs...)
+        end
+        eng_sampler = _dict[:energy_sampler]
+        if choose_lowest_energy
+            nmin = minimum([nmin, size(eng_sampler.buffer)[end]])
+            xmin = ECCCo.get_lowest_energy_sample(eng_sampler; n=nmin)
+            push!(conditional_samples, xmin)
+        elseif choose_random
+            push!(conditional_samples, rand(eng_sampler, n; from_buffer=from_buffer))
+        else
+            push!(conditional_samples, eng_sampler.buffer)
+        end
+    end
+
+    xtarget = conditional_samples[1]                    # conditional samples
+    x = CounterfactualExplanations.decode_state(ce)     # current state
+    E(x) = -logits(ce.M, x)[ce.target,:]                # negative logits for target class
+    _loss = E(x) .- E(xtarget)
+
+    _loss = reduce((x, y) -> x + y, _loss) / n          # aggregate over samples
+
+    if return_conditionals
+        return conditional_samples[1]
+    end
+    return _loss
+
+end
+
 function distance_from_energy(
     ce::AbstractCounterfactualExplanation;
     n::Int=50, niter=500, from_buffer=true, agg=mean, 
