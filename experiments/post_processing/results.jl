8864e4dd05e2c74f5ed8d497f01d6b52aa2ed45e
Pat Alt <55311242+pat-alt@users.noreply.github.com>
2023-09-08 13:17:40 +0200

    added bash file and updated deps

---
+++ experiments/post_processing/results.jl
@@ -0,0 +1,14 @@
+"""
+    aggregate(outcome::ExperimentOutcome; measure::String="distance_from_targets")
+
+Function to quickly aggregate benchmarking results for a given measure.
+"""
+function aggregate(outcome::ExperimentOutcome; measure::String="distance_from_targets")
+    df = @chain outcome.bmk() begin
+        @group_by(generator, model)
+        @filter(variable == measure)
+        @summarize(val = mean(value), std = std(value))
+        @ungroup
+    end
+    return df
+end
