0cc0216b11dedb5a7fc17bba42d781d682947654
Pat Alt <55311242+pat-alt@users.noreply.github.com>
2023-09-13 14:05:48 +0200

    avoid threading because gather is apparently not thread safe

--- experiments/gmsc.sh
+++ experiments/gmsc.sh
@@ -2,8 +2,8 @@
 
 #SBATCH --job-name="GMSC (ECCCo)"
 #SBATCH --time=3:00:00
-#SBATCH --ntasks=12
-#SBATCH --cpus-per-task=4
+#SBATCH --ntasks=48
+#SBATCH --cpus-per-task=1
 #SBATCH --partition=compute
 #SBATCH --mem-per-cpu=4GB
 #SBATCH --account=innovation
@@ -11,4 +11,4 @@
 
 module load 2023r1 openmpi
 
-srun julia --project=experiments --threads 4 experiments/run_experiments.jl -- data=gmsc output_path=results retrain threaded mpi > experiments/gmsc.log
+srun julia --project=experiments experiments/run_experiments.jl -- data=gmsc output_path=results retrain mpi > experiments/gmsc.log
--- experiments/synthetic.sh
+++ experiments/synthetic.sh
@@ -2,8 +2,8 @@
 
 #SBATCH --job-name="Synthetic (ECCCo)"
 #SBATCH --time=02:00:00
-#SBATCH --ntasks=12
-#SBATCH --cpus-per-task=4
+#SBATCH --ntasks=48
+#SBATCH --cpus-per-task=1
 #SBATCH --partition=compute
 #SBATCH --mem-per-cpu=4GB
 #SBATCH --account=innovation
@@ -11,4 +11,4 @@
 
 module load 2023r1 openmpi
 
-srun julia --project=experiments --threads 4 experiments/run_experiments.jl -- data=linearly_separable,moons,circles output_path=results threaded mpi retrain > experiments/synthetic.log
+srun julia --project=experiments experiments/run_experiments.jl -- data=linearly_separable,moons,circles output_path=results mpi retrain > experiments/synthetic.log
