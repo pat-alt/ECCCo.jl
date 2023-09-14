fd438e4be923aa697ce79bd92a28e59f89f4681d
Pat Alt <55311242+pat-alt@users.noreply.github.com>
2023-09-14 07:24:41 +0200

    fixed

--- experiments/jobscripts/models/german_credit.sh
+++ experiments/jobscripts/models/german_credit.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks=1
 #SBATCH --gpus-per-task=1
 #SBATCH --cpus-per-task=1
-#SBATCH --partition=compute
+#SBATCH --partition=gpu
 #SBATCH --mem-per-cpu=4GB
 #SBATCH --account=innovation
 #SBATCH --mail-type=END     # Set mail type to 'END' to receive a mail when the job finishes. 
