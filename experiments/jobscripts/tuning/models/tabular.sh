5139879ae5f7d582d79117b12137f0c09fa2ccd2
Pat Alt <55311242+pat-alt@users.noreply.github.com>
2023-09-15 16:48:24 +0200

    model tuning to innovation

--- experiments/jobscripts/tuning/models/tabular.sh
+++ experiments/jobscripts/tuning/models/tabular.sh
@@ -6,8 +6,8 @@
 #SBATCH --gpus-per-task=1
 #SBATCH --cpus-per-task=1
 #SBATCH --partition=gpu
-#SBATCH --mem-per-cpu=8GB
-#SBATCH --account=research-eemcs-insy
+#SBATCH --mem-per-cpu=4GB
+#SBATCH --account=innovation
 #SBATCH --mail-type=END     # Set mail type to 'END' to receive a mail when the job finishes.
 
 srun julia --project=experiments experiments/run_experiments.jl -- data=gmsc,german_credit,credit_default,california_housing output_path=results tune_model
