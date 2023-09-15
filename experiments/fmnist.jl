0044714e3f506ac304c7c4a163d14f84573dce7d
Pat Alt <55311242+pat-alt@users.noreply.github.com>
2023-09-15 17:27:23 +0200

    fashion mnist

--- experiments/fmnist.jl
+++ experiments/fmnist.jl
@@ -1,4 +1,5 @@
 # Training data:
+dataname = "Fashion MNIST"
 n_obs = 10000
 counterfactual_data = load_fashion_mnist(n_obs)
 counterfactual_data.X = ECCCo.pre_process.(counterfactual_data.X)
@@ -11,37 +12,46 @@ counterfactual_data.generative_model = vae
 # Test data:
 test_data = load_fashion_mnist_test()
 
+# Model tuning:
+model_tuning_params = DEFAULT_MODEL_TUNING_LARGE
+
+# Tuning parameters:
+tuning_params = DEFAULT_GENERATOR_TUNING
+push!(tuning_params.Λ, [0.1, 0.1, 3.0])
+
 # Additional models:
 add_models = Dict(
     "LeNet-5" => lenet5,
+    "ResNet-18" => resnet18(; epochs=10),
 )
 
-# Default builder:
-n_hidden = 128
-activation = Flux.swish
-builder = MLJFlux.@builder Flux.Chain(
-    Dense(n_in, n_hidden, activation),
-    Dense(n_hidden, n_out),
-)
-
-# Number of individuals:
-n_ind = N_IND_SPECIFIED ? N_IND : 5
-
-# Run:
-run_experiment(
-    counterfactual_data, test_data; 
-    dataname="Fashion-MNIST",
-    builder= builder,
-    𝒟x = Uniform(-1.0, 1.0),
-    α = [1.0,1.0,1e-2],
-    sampling_batch_size = 10,
+# Parameter choices:
+params = (
+    n_ind=N_IND_SPECIFIED ? N_IND : 10,
+    builder=default_builder(n_hidden=128, n_layers=2, activation=Flux.swish),
+    𝒟x=Uniform(-1.0, 1.0),
+    α=[1.0, 1.0, 1e-2],
+    sampling_batch_size=10,
     sampling_steps=50,
-    use_ensembling = true,
-    n_individuals = n_ind,
-    nsamples = 10,
-    nmin = 10,
-    use_variants = false,
-    use_class_loss = true,
+    use_ensembling=true,
+    use_variants=false,
     additional_models=add_models,
-    epochs = 10,
+    epochs=10,
+    nsamples=10,
+    nmin=1,
+    niter_eccco=100
 )
+
+if !GRID_SEARCH
+    run_experiment(
+        counterfactual_data, test_data;
+        dataname=dataname,
+        params...
+    )
+else
+    grid_search(
+        counterfactual_data, test_data;
+        dataname=dataname,
+        tuning_params=tuning_params
+    )
+end
