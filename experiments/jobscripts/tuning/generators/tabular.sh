16a7021bb4438481b48b3b84bddf7f2be0fd688b
Pat Alt <55311242+pat-alt@users.noreply.github.com>
2023-09-17 13:11:54 +0200

    increased mem for tabular

--- experiments/jobscripts/tuning/generators/tabular.sh
+++ experiments/jobscripts/tuning/generators/tabular.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks=1000
 #SBATCH --cpus-per-task=1
 #SBATCH --partition=compute
-#SBATCH --mem-per-cpu=8GB
+#SBATCH --mem-per-cpu=12GB
 #SBATCH --account=research-eemcs-insy
 #SBATCH --mail-type=END     # Set mail type to 'END' to receive a mail when the job finishes. 
 
