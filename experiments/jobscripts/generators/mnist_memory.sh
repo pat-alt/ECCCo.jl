a482cec1c8d08377a7548196b27b7940f2088b01
Pat Alt <55311242+pat-alt@users.noreply.github.com>
2023-09-18 16:30:54 +0200

    high mem, many cores

--- experiments/jobscripts/generators/mnist_memory.sh
+++ experiments/jobscripts/generators/mnist_memory.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --job-name="MNIST (ECCCo)"
 #SBATCH --time=10:00:00
-#SBATCH --ntasks=100
+#SBATCH --ntasks=500
 #SBATCH --cpus-per-task=1
 #SBATCH --partition=memory
 #SBATCH --mem-per-cpu=32GB
