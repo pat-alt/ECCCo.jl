ce3c89e20735abfd6e3403e2cc4cc4cecc8f23c0
Pat Alt <55311242+pat-alt@users.noreply.github.com>
2023-09-18 17:14:56 +0200

    uh

--- experiments/jobscripts/tuning/generators/synthetic.sh
+++ experiments/jobscripts/tuning/generators/synthetic.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --job-name="Grid-search Synthetic (ECCCo)"
-#SBATCH --time=02:00:00
+#SBATCH --time=05:00:00
 #SBATCH --ntasks=1000
 #SBATCH --cpus-per-task=1
 #SBATCH --partition=compute
