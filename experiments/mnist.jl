e3bfed0c48c6d0685f910e631418df24cb5803ec
Pat Alt <55311242+pat-alt@users.noreply.github.com>
2023-09-19 08:44:55 +0200

    time increased for tabular and cali

--- experiments/mnist.jl
+++ experiments/mnist.jl
@@ -41,7 +41,8 @@ params = (
     niter_eccco=500,
     Λ=[0.1, 0.25, 0.25],
     Λ_Δ=[0.1, 0.1, 2.5],
-    opt=Flux.Optimise.Adam(0.1)
+    opt=Flux.Optimise.Descent(0.1),
+    reg_strength = 0
 )
 
 if !GRID_SEARCH
