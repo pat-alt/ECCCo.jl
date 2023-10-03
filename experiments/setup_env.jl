b36ec996428a81eb4749a2c5cd8d18463ef42854
Pat Alt <55311242+pat-alt@users.noreply.github.com>
2023-10-03 13:49:00 +0200

    batching

--- experiments/setup_env.jl
+++ experiments/setup_env.jl
@@ -47,6 +47,34 @@ include("post_processing/post_processing.jl")
 include("utils.jl")
 include("save_best.jl")
 
+# Number of counterfactuals:
+n_ind_specified = false
+if any(contains.(ARGS, "n_individuals="))
+    n_ind_specified = true
+    n_individuals =
+        ARGS[findall(contains.(ARGS, "n_individuals="))][1] |>
+        x -> replace(x, "n_individuals=" => "") |> x -> parse(Int, x)
+else
+    n_individuals = 100
+end
+
+"Number of individuals to use in benchmarking."
+const N_IND = n_individuals
+
+"Boolean flag to check if number of individuals was specified."
+const N_IND_SPECIFIED = n_ind_specified
+
+if any(contains.(ARGS, "n_each="))
+    n_each =
+        ARGS[findall(contains.(ARGS, "n_each="))][1] |>
+        x -> replace(x, "n_each=" => "") |> x -> parse(Int, x)
+else
+    n_each = 16
+end
+
+"Number of objects to pass to each process."
+const N_EACH = n_each
+
 # Parallelization:
 plz = nothing
 
@@ -60,7 +88,7 @@ end
 if "mpi" ∈ ARGS
     MPI.Init()
     const USE_MPI = true
-    plz = MPIParallelizer(MPI.COMM_WORLD; threaded = USE_THREADS)
+    plz = MPIParallelizer(MPI.COMM_WORLD; threaded = USE_THREADS, n_each = N_EACH)
     if MPI.Comm_rank(MPI.COMM_WORLD) != 0
         global_logger(NullLogger())
     else
@@ -134,22 +162,6 @@ const TEST_SIZE = 0.2
 
 "Boolean flag to check if upload was specified."
 const UPLOAD = "upload" ∈ ARGS
-
-n_ind_specified = false
-if any(contains.(ARGS, "n_individuals="))
-    n_ind_specified = true
-    n_individuals =
-        ARGS[findall(contains.(ARGS, "n_individuals="))][1] |>
-        x -> replace(x, "n_individuals=" => "") |> x -> parse(Int, x)
-else
-    n_individuals = 100
-end
-
-"Number of individuals to use in benchmarking."
-const N_IND = n_individuals
-
-"Boolean flag to check if number of individuals was specified."
-const N_IND_SPECIFIED = n_ind_specified
 
 "Boolean flag to check if grid search was specified."
 const GRID_SEARCH = "grid_search" ∈ ARGS
