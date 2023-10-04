8f5349bbe06be9850b75cabc977e02704d27d568
Pat Alt <55311242+pat-alt@users.noreply.github.com>
2023-10-04 15:10:43 +0200

    testing

--- experiments/daic/testing/lin_sep.sh
+++ experiments/daic/testing/lin_sep.sh
@@ -2,8 +2,8 @@
 
 #SBATCH --job-name="Grid-search Linearly Separable (ECCCo)"
 #SBATCH --time=01:00:00
-#SBATCH --ntasks=100
-#SBATCH --cpus-per-task=1
+#SBATCH --ntasks-per-node=5
+#SBATCH --cpus-per-task=4
 #SBATCH --partition=general
 #SBATCH --mem-per-cpu=4GB
 #SBATCH --mail-type=END     # Set mail type to 'END' to receive a mail when the job finishes. 
@@ -11,4 +11,6 @@
 module use /opt/insy/modulefiles          # Use DAIC INSY software collection
 module load openmpi
 
-srun julia --project=experiments experiments/run_experiments.jl -- data=linearly_separable output_path=results mpi grid_search n_individuals=10 > experiments/grid_search_linearly_separable.log
+export SRUN_CPUS_PER_TASK="$SLURM_CPUS_PER_TASK"
+
+srun julia --project=experiments experiments/run_experiments.jl -- data=linearly_separable output_path=results mpi grid_search n_individuals=10 threaded > experiments/grid_search_linearly_separable.log
