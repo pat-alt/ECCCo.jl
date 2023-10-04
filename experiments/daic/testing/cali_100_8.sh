b83215b63f884fe2cc2b0a88c458c81da28faeb2
Pat Alt <55311242+pat-alt@users.noreply.github.com>
2023-10-04 14:19:59 +0200

    uf

--- experiments/daic/testing/cali_100_8.sh
+++ experiments/daic/testing/cali_100_8.sh
@@ -5,11 +5,11 @@
 #SBATCH --ntasks=100
 #SBATCH --cpus-per-task=1
 #SBATCH --partition=general
-#SBATCH --mem-per-cpu=4GB
+#SBATCH --mem-per-cpu=8GB
 #SBATCH --mail-type=END     # Set mail type to 'END' to receive a mail when the job finishes. 
 
 module use /opt/insy/modulefiles          # Use DAIC INSY software collection
 module load openmpi
 
-srun julia --project=experiments experiments/run_experiments.jl -- data=california_housing output_path=results mpi grid_search n_individuals=10 n_each=4 > experiments/grid_search_california_housing.log
+srun julia --project=experiments experiments/run_experiments.jl -- data=california_housing output_path=results mpi grid_search n_individuals=10 n_each=16 > experiments/grid_search_california_housing.log
 
--- experiments/daic/testing/cali_50_8.sh
+++ experiments/daic/testing/cali_50_8.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 #SBATCH --job-name="Grid-search California Housing (ECCCo)"
-#SBATCH --time=01:00:00
-#SBATCH --ntasks=100
+#SBATCH --time=02:00:00
+#SBATCH --ntasks=50
 #SBATCH --cpus-per-task=1
 #SBATCH --partition=general
-#SBATCH --mem-per-cpu=4GB
+#SBATCH --mem-per-cpu=8GB
 #SBATCH --mail-type=END     # Set mail type to 'END' to receive a mail when the job finishes. 
 
 module use /opt/insy/modulefiles          # Use DAIC INSY software collection
 module load openmpi
 
-srun julia --project=experiments experiments/run_experiments.jl -- data=california_housing output_path=results mpi grid_search n_individuals=10 n_each=32 > experiments/grid_search_california_housing.log
+srun julia --project=experiments experiments/run_experiments.jl -- data=california_housing output_path=results mpi grid_search n_individuals=10 n_each=16 > experiments/grid_search_california_housing.log
 
