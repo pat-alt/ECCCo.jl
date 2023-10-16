be84f6be416233a9858538fe0e1c5fe37efd2eb8
pat-alt <altmeyerpat@gmail.com>
2023-10-16 17:05:29 +0200

    threads

--- experiments/daic/testing/cali_100_8.sh
+++ experiments/daic/testing/cali_100_8.sh
@@ -13,5 +13,5 @@ module load openmpi
 
 source experiments/slurm_header.sh
 
-srun julia --project=experiments experiments/run_experiments.jl -- data=california_housing output_path=results mpi grid_search n_individuals=10 threaded > experiments/grid_search_california_housing.log
+srun julia --project=experiments --threads $SLURM_CPUS_PER_TASK experiments/run_experiments.jl -- data=california_housing output_path=results mpi grid_search n_individuals=10 threaded > experiments/grid_search_california_housing.log
 
