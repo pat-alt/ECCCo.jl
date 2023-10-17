b597c48a4f7d6910156c7520525d5d77471035f0
pat-alt <altmeyerpat@gmail.com>
2023-10-17 15:00:24 +0200

    tabular

--- experiments/jobscripts/tuning/generators/california_housing.sh
+++ experiments/jobscripts/tuning/generators/california_housing.sh
@@ -13,4 +13,4 @@ module load 2023r1 openmpi
 
 source experiments/slurm_header.sh
 
-srun julia --project=experiments --threads $SLURM_CPUS_PER_TASK experiments/run_experiments.jl -- data=california_housing output_path=results mpi grid_search threaded n_individuals=10 > experiments/logs/grid_search_california_housing.log
+srun julia --project=experiments --threads $SLURM_CPUS_PER_TASK experiments/run_experiments.jl -- data=california_housing output_path=results mpi grid_search threaded n_individuals=25 > experiments/logs/grid_search_california_housing.log
