ef514312c83337550e75a86c7483f94f055c93a5
pat-alt <altmeyerpat@gmail.com>
2023-10-17 09:20:33 +0200

    uhuhuh

--- experiments/jobscripts/testing/cali.sh
+++ experiments/jobscripts/testing/cali.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks=14
 #SBATCH --cpus-per-task=14
 #SBATCH --partition=compute
-#SBATCH --mem-per-cpu=2GB
+#SBATCH --mem-per-cpu=1GB
 #SBATCH --account=research-eemcs-insy
 #SBATCH --mail-type=END     # Set mail type to 'END' to receive a mail when the job finishes. 
 
@@ -14,4 +14,4 @@ module load openmpi
 
 source experiments/slurm_header.sh
 
-srun julia --project=experiments --threads $SLURM_CPUS_PER_TASK experiments/run_experiments.jl -- data=california_housing output_path=results mpi grid_search n_individuals=10 threaded > experiments/grid_search_california_housing.log
+srun julia --project=experiments --threads $SLURM_CPUS_PER_TASK experiments/run_experiments.jl -- data=california_housing output_path=results mpi grid_search n_individuals=10 n_each=$SLURM_CPUS_PER_TASK threaded > experiments/grid_search_california_housing.log
