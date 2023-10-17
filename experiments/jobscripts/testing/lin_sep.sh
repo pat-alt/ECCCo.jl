d6d7524916008816df029653b7c61a99e3c7f34c
pat-alt <altmeyerpat@gmail.com>
2023-10-17 11:40:44 +0200

    scaling up

--- experiments/jobscripts/testing/lin_sep.sh
+++ experiments/jobscripts/testing/lin_sep.sh
@@ -2,10 +2,10 @@
 
 #SBATCH --job-name="Grid-search Linearly Separable (ECCCo)"
 #SBATCH --time=00:30:00
-#SBATCH --ntasks=200
-#SBATCH --cpus-per-task=1
+#SBATCH --ntasks=100
+#SBATCH --cpus-per-task=5
 #SBATCH --partition=compute
-#SBATCH --mem-per-cpu=4GB
+#SBATCH --mem-per-cpu=1GB
 #SBATCH --account=research-eemcs-insy
 #SBATCH --mail-type=END     # Set mail type to 'END' to receive a mail when the job finishes. 
 
@@ -13,5 +13,5 @@ module load 2023r1 openmpi
 
 source experiments/slurm_header.sh
 
-srun julia --project=experiments experiments/run_experiments.jl -- data=linearly_separable output_path=results mpi grid_search n_individuals=20 > experiments/logs/grid_search_linearly_separable.log
+srun julia --project=experiments experiments/run_experiments.jl -- data=linearly_separable output_path=results mpi grid_search n_individuals=50 > experiments/logs/grid_search_linearly_separable.log
  
