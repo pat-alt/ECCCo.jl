1e7b13981a633cfa6f305c833d19192ae9f113bd
pat-alt <altmeyerpat@gmail.com>
2023-10-18 19:59:18 +0200

    bleh

--- experiments/jobscripts/testing/cali.sh
+++ experiments/jobscripts/testing/cali.sh
@@ -2,8 +2,8 @@
 
 #SBATCH --job-name="Grid-search California Housing (ECCCo)"
 #SBATCH --time=00:35:00
-#SBATCH --ntasks=14
-#SBATCH --cpus-per-task=14
+#SBATCH --ntasks=5
+#SBATCH --cpus-per-task=5
 #SBATCH --partition=compute
 #SBATCH --mem-per-cpu=1GB
 #SBATCH --account=research-eemcs-insy
@@ -13,4 +13,4 @@ module load 2023r1 openmpi
 
 source experiments/slurm_header.sh
 
-srun julia --project=experiments --threads $SLURM_CPUS_PER_TASK experiments/run_experiments.jl -- data=california_housing output_path=results mpi grid_search n_individuals=10 n_each=70 threaded > experiments/logs/grid_search_california_housing.log
+srun julia --project=experiments --threads $SLURM_CPUS_PER_TASK experiments/run_experiments.jl -- data=california_housing output_path=results mpi grid_search n_individuals=1 threaded > experiments/logs/grid_search_california_housing.log
