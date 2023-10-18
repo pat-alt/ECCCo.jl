137d3143abb61351eb7f21b9c97f63fcc9b6413a
pat-alt <altmeyerpat@gmail.com>
2023-10-18 12:56:45 +0200

    let's see

--- experiments/jobscripts/tuning/generators/gmsc.sh
+++ experiments/jobscripts/tuning/generators/gmsc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --job-name="Grid-search GMSC (ECCCo)"
-#SBATCH --time=01:10:00
+#SBATCH --time=01:30:00
 #SBATCH --ntasks=15
 #SBATCH --cpus-per-task=10
 #SBATCH --partition=compute
--- experiments/jobscripts/tuning/generators/moons.sh
+++ experiments/jobscripts/tuning/generators/moons.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --job-name="Grid-search Moons (ECCCo)"
-#SBATCH --time=01:00:00
+#SBATCH --time=01:30:00
 #SBATCH --ntasks=15
 #SBATCH --cpus-per-task=10
 #SBATCH --partition=compute
