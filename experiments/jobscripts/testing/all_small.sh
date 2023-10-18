e6d0ea47460bbabb566e4ce8abb08c607a03ec1c
pat-alt <altmeyerpat@gmail.com>
2023-10-18 18:11:30 +0200

    test script

---
+++ experiments/jobscripts/testing/all_small.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --job-name="Grid-search Linearly Separable (ECCCo)"
+#SBATCH --time=01:00:00
+#SBATCH --ntasks=5
+#SBATCH --cpus-per-task=4
+#SBATCH --partition=compute
+#SBATCH --mem-per-cpu=2GB
+#SBATCH --account=research-eemcs-insy
+#SBATCH --mail-type=END     # Set mail type to 'END' to receive a mail when the job finishes. 
+
+module load 2023r1 openmpi
+
+source experiments/slurm_header.sh
+
+srun julia --project=experiments --threads $SLURM_CPUS_PER_TASK experiments/run_experiments.jl -- data=linearly_separable,moons,circles,gmsc,german_credit,california_housing output_path=results_testing mpi grid_search n_individuals=5 threaded > experiments/logs/all_testing.log
+ 
