ebf4b0c15cfc5a173e3c442e5f5afb610e7d1c85
pat-alt <altmeyerpat@gmail.com>
2023-10-19 08:05:39 +0200

    mnist causes segfault on daic but not locally

--- experiments/jobscripts/tuning/generators/mnist.sh
+++ experiments/jobscripts/tuning/generators/mnist.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #SBATCH --job-name="Grid-search MNIST (ECCCo)"
-#SBATCH --time=32:00:00
-#SBATCH --ntasks=1000
-#SBATCH --cpus-per-task=1
+#SBATCH --time=02:00:00
+#SBATCH --ntasks=40
+#SBATCH --cpus-per-task=10
 #SBATCH --partition=compute
 #SBATCH --mem-per-cpu=8GB
 #SBATCH --account=research-eemcs-insy
@@ -11,4 +11,6 @@
 
 module load 2023r1 openmpi
 
-srun julia --project=experiments experiments/run_experiments.jl -- data=mnist output_path=results mpi grid_search > experiments/grid_search_mnist.log
+source experiments/slurm_header.sh
+
+srun julia --project=experiments --threads $SLURM_CPUS_PER_TASK experiments/run_experiments.jl -- data=mnist output_path=results mpi grid_search threaded n_individuals=1 n_each=10 > experiments/logs/grid_search_mnist.log
