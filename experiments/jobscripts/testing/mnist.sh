ef17b1cbcba79e239bd4557137f2b1f5f2aba2b6
pat-alt <altmeyerpat@gmail.com>
2023-11-07 10:46:06 +0100

    adding tester

--- experiments/jobscripts/testing/mnist.sh
+++ experiments/jobscripts/testing/mnist.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#SBATCH --job-name="MNIST test (ECCCo)"
-#SBATCH --time=00:30:00
-#SBATCH --ntasks=10
+#SBATCH --job-name="MNIST (ECCCo)"
+#SBATCH --time=01:00:00
+#SBATCH --ntasks=5
 #SBATCH --cpus-per-task=4
 #SBATCH --partition=compute
-#SBATCH --mem-per-cpu=8GB
+#SBATCH --mem-per-cpu=16GB
 #SBATCH --account=research-eemcs-insy
 #SBATCH --mail-type=END     # Set mail type to 'END' to receive a mail when the job finishes. 
 
@@ -13,5 +13,4 @@ module load 2023r1 openmpi
 
 source experiments/slurm_header.sh
 
-srun julia --project=experiments --threads $SLURM_CPUS_PER_TASK experiments/run_experiments.jl -- data=mnist output_path=results_testing mpi grid_search n_individuals=10 threaded n_each=32 > experiments/logs/grid_search_mnist.log
- 
+srun julia --project=experiments --threads $SLURM_CPUS_PER_TASK experiments/run_experiments.jl -- data=mnist output_path=results mpi threaded n_individuals=50 n_runs=5 n_each=16 > experiments/logs/mnist.log
