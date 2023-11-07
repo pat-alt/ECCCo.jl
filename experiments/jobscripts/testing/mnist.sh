484ec9d7105a43c58a270ebc834183434a973c7a
pat-alt <altmeyerpat@gmail.com>
2023-11-07 12:50:13 +0100

    trying different specs

--- experiments/jobscripts/testing/mnist.sh
+++ experiments/jobscripts/testing/mnist.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name="MNIST (ECCCo)"
 #SBATCH --time=01:00:00
 #SBATCH --ntasks=5
-#SBATCH --cpus-per-task=10
+#SBATCH --cpus-per-task=4
 #SBATCH --partition=compute
 #SBATCH --mem-per-cpu=16GB
 #SBATCH --account=research-eemcs-insy
@@ -13,4 +13,4 @@ module load 2023r1 openmpi
 
 source experiments/slurm_header.sh
 
-srun julia --project=experiments --threads $SLURM_CPUS_PER_TASK experiments/run_experiments.jl -- data=mnist output_path=results mpi threaded n_individuals=50 n_runs=5 n_each=16 > experiments/logs/mnist.log
+srun julia --project=experiments --threads $SLURM_CPUS_PER_TASK experiments/run_experiments.jl -- data=mnist output_path=results mpi threaded n_individuals=48 n_runs=5 n_each=8 > experiments/logs/mnist.log
