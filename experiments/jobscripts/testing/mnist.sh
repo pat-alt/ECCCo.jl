3b8790a20637782b76f40087f4fae9cc0d9ddd85
pat-alt <altmeyerpat@gmail.com>
2023-11-10 08:22:40 +0100

    trying 20

--- experiments/jobscripts/testing/mnist.sh
+++ experiments/jobscripts/testing/mnist.sh
@@ -13,4 +13,4 @@ module load 2023r1 openmpi
 
 source experiments/slurm_header.sh
 
-srun julia --project=experiments --threads $SLURM_CPUS_PER_TASK experiments/run_experiments.jl -- data=mnist output_path=results mpi threaded n_individuals=25 n_runs=5 n_each=16 > experiments/logs/mnist.log
+srun julia --project=experiments --threads $SLURM_CPUS_PER_TASK experiments/run_experiments.jl -- data=mnist output_path=results mpi threaded n_individuals=20 n_runs=5 n_each=16 > experiments/logs/mnist.log
