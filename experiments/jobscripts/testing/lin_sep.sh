cb5acae90ea0d936be18f8562a8510d6191d9526
pat-alt <altmeyerpat@gmail.com>
2023-11-14 10:10:51 +0100

    f

--- experiments/jobscripts/testing/lin_sep.sh
+++ experiments/jobscripts/testing/lin_sep.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#SBATCH --job-name="MNIST (ECCCo)"
-#SBATCH --time=01:00:00
+#SBATCH --job-name="Test Lin Sep (ECCCo)"
+#SBATCH --time=00:20:00
 #SBATCH --ntasks=5
 #SBATCH --cpus-per-task=4
 #SBATCH --partition=compute
@@ -13,4 +13,4 @@ module load 2023r1 openmpi
 
 source experiments/slurm_header.sh
 
-srun julia --project=experiments --threads $SLURM_CPUS_PER_TASK experiments/run_experiments.jl -- data=linearly_separable output_path=results mpi threaded n_individuals=48 n_runs=5 n_each=8 > experiments/logs/mnist.log
+srun julia --project=experiments --threads $SLURM_CPUS_PER_TASK experiments/run_experiments.jl -- data=linearly_separable output_path=results mpi threaded n_individuals=48 n_runs=5 n_each=8 > experiments/logs/lin-sep.log
