ff9c7aca49906ca63486f8c8870f4533612355dd
pat-alt <altmeyerpat@gmail.com>
2023-12-06 10:35:28 +0100

    hu

--- experiments/jobscripts/testing/mnist.sh
+++ experiments/jobscripts/testing/mnist.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #SBATCH --job-name="MNIST (ECCCo)"
-#SBATCH --time=01:00:00
-#SBATCH --ntasks=10
-#SBATCH --cpus-per-task=8
+#SBATCH --time=00:10:00
+#SBATCH --ntasks=30
+#SBATCH --cpus-per-task=10
 #SBATCH --partition=compute
-#SBATCH --mem-per-cpu=8GB
+#SBATCH --mem-per-cpu=4GB
 #SBATCH --account=research-eemcs-insy
 #SBATCH --mail-type=END     # Set mail type to 'END' to receive a mail when the job finishes. 
 
