57d7851d53fbe11027f341206f0415123d55d9d0
pat-alt <altmeyerpat@gmail.com>
2023-12-06 13:55:15 +0100

    uh

--- experiments/jobscripts/generators/mnist.sh
+++ experiments/jobscripts/generators/mnist.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks=30
 #SBATCH --cpus-per-task=10
 #SBATCH --partition=compute
-#SBATCH --mem-per-cpu=16GB
+#SBATCH --mem-per-cpu=4GB
 #SBATCH --account=research-eemcs-insy
 #SBATCH --mail-type=END     # Set mail type to 'END' to receive a mail when the job finishes. 
 
@@ -13,4 +13,4 @@ module load 2023r1 openmpi
 
 source experiments/slurm_header.sh
 
-srun julia --project=experiments --threads $SLURM_CPUS_PER_TASK experiments/run_experiments.jl -- data=mnist output_path=results mpi threaded n_individuals=50 n_runs=5 vertical_splits=50 > experiments/logs/mnist.log
+srun julia --project=experiments --threads $SLURM_CPUS_PER_TASK experiments/run_experiments.jl -- data=mnist output_path=results mpi threaded n_individuals=100 n_runs=5 vertical_splits=100 > experiments/logs/mnist.log
