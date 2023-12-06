2ac2dbd0656ba3268e9ecbcfd592d2e56e81fb93
pat-alt <altmeyerpat@gmail.com>
2023-12-06 07:49:57 +0100

    100 individuals 5 runs 80 cores

--- experiments/jobscripts/testing/mnist.sh
+++ experiments/jobscripts/testing/mnist.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name="MNIST (ECCCo)"
 #SBATCH --time=01:00:00
 #SBATCH --ntasks=10
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=8
 #SBATCH --partition=compute
 #SBATCH --mem-per-cpu=8GB
 #SBATCH --account=research-eemcs-insy
@@ -13,4 +13,4 @@ module load 2023r1 openmpi
 
 source experiments/slurm_header.sh
 
-srun julia --project=experiments --threads $SLURM_CPUS_PER_TASK experiments/run_experiments.jl -- data=mnist output_path=results_testing mpi threaded n_individuals=50 n_runs=5 vertical_splits=50 > experiments/logs/testing_mnist.log
+srun julia --project=experiments --threads $SLURM_CPUS_PER_TASK experiments/run_experiments.jl -- data=mnist output_path=results_testing mpi threaded n_individuals=100 n_runs=5 vertical_splits=100 > experiments/logs/testing_mnist.log
