5820d868800b01217f996c79436d58239081f8ea
pat-alt <altmeyerpat@gmail.com>
2023-12-07 07:48:52 +0100

    testing fmnist

--- experiments/jobscripts/generators/fmnist.sh
+++ experiments/jobscripts/generators/fmnist.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks=30
 #SBATCH --cpus-per-task=20
 #SBATCH --partition=compute
-#SBATCH --mem-per-cpu=4GB
+#SBATCH --mem-per-cpu=6GB
 #SBATCH --account=research-eemcs-insy
 #SBATCH --mail-type=END     # Set mail type to 'END' to receive a mail when the job finishes. 
 
